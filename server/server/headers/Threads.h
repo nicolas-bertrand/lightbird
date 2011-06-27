nicolas-bertrand/lightbird

commit 51e6640f37ab91726c9219091573413852b1838b

Replaces the tryLock by the SmartMutex.

server/server/headers/Threads.h
@@ -36,15 +36,15 @@ private:
     Threads &operator=(const Threads &);
     ~Threads();
 
-    QMap<QThread *, bool>   threads;        ///< The list of the managed threads. The value tells if the object has to be deleted when the thread is finished.
-    QMutex                  lockThreads;    ///< Lock the member threads.
-    static Threads          *_instance;     ///< The instance of the singleton
+    QMap<QThread *, bool>   threads;    ///< The list of the managed threads. The value tells if the object has to be deleted when the thread is finished.
+    QMutex                  mutex;      ///< Lock the member threads.
+    static Threads          *_instance; ///< The instance of the singleton
 
 private slots:
     /// @brief Clean a thread that has finished.
     void                _threadFinished();
     /// @brief Clean a thread that has been destroyed.
-    void                _threadDestroyed(QObject *object);
+    void                _threadDestroyed(QThread *object);
 };
 
 #endif // THREADS_H

server/server/sources/Threads.cpp
@@ -1,5 +1,6 @@
 #include "Defines.h"
 #include "Log.h"
+#include "SmartMutex.h"
 #include "Threads.h"
 
 Threads *Threads::_instance = NULL;
@@ -29,13 +30,12 @@ Threads *Threads::instance(QObject *parent)
     return (_instance);
 }
 
-void    Threads::newThread(QThread *thread, bool remove)
+void            Threads::newThread(QThread *thread, bool remove)
 {
-    if (!this->lockThreads.tryLock(MAXTRYLOCK))
-    {
-        Log::error("Deadlock", "Threads", "newThread");
+    SmartMutex  mutex(this->mutex, "Threads", "newThread");
+
+    if (!mutex)
         return ;
-    }
     if (this->threads.contains(thread))
         Log::warning("The thread is already handled", Properties("thread", QString::number((quint64)thread, 16)), "Threads", "newThread");
     else if (thread->isFinished())
@@ -54,38 +54,31 @@ void    Threads::newThread(QThread *thread, bool remove)
         if (thread->isRunning() == false)
             thread->start();
     }
-    this->lockThreads.unlock();
 }
 
-void    Threads::deleteThread(QThread *thread)
+void            Threads::deleteThread(QThread *thread)
 {
-    if (!this->lockThreads.tryLock(MAXTRYLOCK))
-    {
-        Log::error("Deadlock", "Threads", "deleteThread");
+    SmartMutex  mutex(this->mutex, "Threads", "deleteThread");
+
+    if (!mutex)
         return ;
-    }
     if (this->threads.contains(thread) == true)
     {
         Log::trace("Deleting the thread", Properties("thread", QString::number((quint64)thread, 16)), "Threads", "deleteThread");
         // Quit the event loop of the thread
         thread->quit();
     }
-    this->lockThreads.unlock();
 }
 
-void        Threads::deleteAll()
+void            Threads::deleteAll()
 {
-    if (!this->lockThreads.tryLock(MAXTRYLOCK))
-    {
-        Log::error("Deadlock", "Threads", "deleteAll");
+    SmartMutex  mutex(this->mutex, "Threads", "deleteAll");
+
+    if (!mutex)
         return ;
-    }
     // If all the threads are already deleted
     if (this->threads.isEmpty())
-    {
-        this->lockThreads.unlock();
         return ;
-    }
     Log::info("Deleting all the threads", "Threads", "deleteAll");
     QMutableMapIterator<QThread *, bool> it(this->threads);
     // Quit the event loop of all the threads
@@ -114,16 +107,14 @@ void        Threads::deleteAll()
         }
     }
     this->threads.clear();
-    this->lockThreads.unlock();
 }
 
-void        Threads::_threadFinished()
+void            Threads::_threadFinished()
 {
-    if (!this->lockThreads.tryLock(MAXTRYLOCK))
-    {
-        Log::error("Deadlock", "Threads", "_threadFinished");
+    SmartMutex  mutex(this->mutex, "Threads", "_threadFinished");
+
+    if (!mutex)
         return ;
-    }
     QMutableMapIterator<QThread *, bool> it(this->threads);
     while (it.hasNext())
     {
@@ -144,16 +135,14 @@ void        Threads::_threadFinished()
             break;
         }
     }
-    this->lockThreads.unlock();
 }
 
-void        Threads::_threadDestroyed(QObject *object)
+void            Threads::_threadDestroyed(QThread *object)
 {
-    if (!this->lockThreads.tryLock(MAXTRYLOCK))
-    {
-        Log::error("Deadlock", "Threads", "_threadDestroyed");
+    SmartMutex  mutex(this->mutex, "Threads", "_threadDestroyed");
+
+    if (!mutex)
         return ;
-    }
     QMutableMapIterator<QThread *, bool> it(this->threads);
     while (it.hasNext())
     {
@@ -166,5 +155,4 @@ void        Threads::_threadDestroyed(QObject *object)
             break;
         }
     }
-    this->lockThreads.unlock();
 }

server/plugins/Http/Get/Get.cpp
@@ -47,10 +47,44 @@ bool    Get::doExecution(LightBird::IClient &client)
 {
     LightBird::IMetadata metadata;
 
-    metadata = this->api->plugins().getMetadata(this->api->getId());
-    QString s = metadata.name;
-    client.getResponse().getContent().setContent(s.toAscii());
+    if (client.getMode() == LightBird::IClient::SERVER)
+    {
+        metadata = this->api->plugins().getMetadata(this->api->getId());
+        QString s = this->api->network().connect(QHostAddress("188.165.253.129"), 80, QStringList("HTTP"))->getResult();
+        if (!this->api->network().send(s))
+            s = "hello world !";
+        else
+        {
+            this->mutex.lock();
+            this->wait.wait(&this->mutex);
+            this->mutex.unlock();
+            s = this->string;
+        }
+        client.getResponse().getContent().setContent(s.toAscii());
+    }
     return (true);
+}
+
+bool    Get::doSend(LightBird::IClient &client)
+{
+    client.getRequest().getHeader().insert("Host", "pcinpact.com");
+    client.getRequest().getHeader().insert("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:2.0.1) Gecko/20100101 Firefox/4.0.1");
+    client.getRequest().getHeader().insert("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
+    client.getRequest().getHeader().insert("Accept-Language", "fr,fr-fr;q=0.8,en-us;q=0.5,en;q=0.3");
+    client.getRequest().getHeader().insert("Accept-Charset", "ISO-8859-1,utf-8;q=0.7,*;q=0.7");
+    client.getRequest().getHeader().insert("Keep-Alive", "115");
+    client.getRequest().getHeader().insert("Connection", "keep-alive");
+    client.getRequest().getHeader().insert("Cache-Control", "max-age=0");
+    return (true);
+}
+
+void    Get::onUnserialize(LightBird::IClient &client, LightBird::IOnUnserialize::Unserialize type)
+{
+    if (client.getMode() == LightBird::IClient::CLIENT && type == LightBird::IOnUnserialize::IDoUnserialize)
+    {
+        this->string = client.getResponse().getContent().getContent();
+        this->wait.wakeAll();
+    }
 }
 
 Q_EXPORT_PLUGIN2(Get, Get)

server/plugins/Http/Get/Get.h
@@ -2,17 +2,25 @@
 # define GET_H
 
 # include <QObject>
+# include <QWaitCondition>
+# include <QMutex>
 
 # include "IDoExecution.h"
+# include "IDoSend.h"
+# include "IOnUnserialize.h"
 # include "IPlugin.h"
 
 class Get : public QObject,
             public LightBird::IPlugin,
-            public LightBird::IDoExecution
+            public LightBird::IDoExecution,
+            public LightBird::IDoSend,
+            public LightBird::IOnUnserialize
 {
     Q_OBJECT
     Q_INTERFACES(LightBird::IPlugin
-                 LightBird::IDoExecution)
+                 LightBird::IDoExecution
+                 LightBird::IDoSend
+                 LightBird::IOnUnserialize)
 
 public:
     Get();
@@ -27,9 +35,14 @@ public:
 
     // Execution
     bool    doExecution(LightBird::IClient &client);
+    bool    doSend(LightBird::IClient &client);
+    void    onUnserialize(LightBird::IClient &client, LightBird::IOnUnserialize::Unserialize type);
 
 private:
     LightBird::IApi *api;
+    QMutex          mutex;
+    QWaitCondition  wait;
+    QString         string;
 };
 
 #endif // GET_H

server/server/headers/Client.h
@@ -98,7 +98,7 @@ private:
     QAbstractSocket          *socket;               ///< An abstract representation of the socket of the client.
     TableAccounts            account;               ///< Allows the client to be identified as a know account.
     QByteArray               *data;                 ///< May contains data read from the network.
-    QMutex                   lockData;              ///< Secure the access to the data member.
+    QMutex                   mutex;                 ///< Secure the access to the data member.
     Engine                   *engine;               ///< Used to process the requests, and generates the responses.
 
     bool            _connectToHost();

server/server/headers/Configuration.h
@@ -65,11 +65,11 @@ protected:
     /// @param root : The root element of the nodeName.
     virtual bool        _remove(const QString &nodeName, QDomElement root);
 
-    QReadWriteLock      domLock;    ///< Make the configuration thread safe.
-    QDomDocument        doc;        ///< The in-memory DOM representation of the XML document.
-    QDomElement         dom;        ///< The root of the XML document.
-    QFile               file;       ///< The XML configuration file.
-    bool                loaded;     ///< If a configuration is loaded.
+    QReadWriteLock      mutex;  ///< Make the configuration thread safe.
+    QDomDocument        doc;    ///< The in-memory DOM representation of the XML document.
+    QDomElement         dom;    ///< The root of the XML document.
+    QFile               file;   ///< The XML configuration file.
+    bool                loaded; ///< If a configuration is loaded.
 
 private slots:
     /// @brief Set the parent of the configuration in his living thread (because this can't be done in an other thread)

server/server/headers/Configurations.h
@@ -39,8 +39,8 @@ public:
     static Configuration    *instance(const QString &path = "", const QString &alternative = "");
 
 private:
-    static QMap<QString, Configuration *>   instances;     ///< The instances of the loaded configurations.
-    static QMutex                           lockInstances; ///< Ensure that instances is thread safe.
+    static QMap<QString, Configuration *>   instances;  ///< The instances of the loaded configurations.
+    static QMutex                           mutex;      ///< Ensure that instances is thread safe.
 };
 
 #endif // CONFIGURATIONS_H

server/server/headers/Network.h
@@ -82,7 +82,7 @@ private:
     Network         *operator=(const Network &);
 
     QMap<unsigned short, Port *>    ports;      ///< The list of the listening ports
-    QReadWriteLock                  lockPorts;  ///< Serure the ports.
+    QReadWriteLock                  mutex;      ///< Serure the ports.
     Clients                         clients;    ///< Manages the clients in CLIENT mode.
     static Network                  *_instance; ///< The instance of the singleton that manage the network.
 

commit ae32147aa21dd05777418ff5269a5b49f05ab587

Fixes a bug introduced in revision 51e6640f37ab91726c9219091573413852b1838b where the slot _threadDestroyed was not connected anymore.

server/server/headers/Threads.h
@@ -44,7 +44,7 @@ private slots:
     /// @brief Clean a thread that has finished.
     void                _threadFinished();
     /// @brief Clean a thread that has been destroyed.
-    void                _threadDestroyed(QThread *object);
+    void                _threadDestroyed(QObject *object);
 };
 
 #endif // THREADS_H

server/server/sources/Threads.cpp
@@ -137,7 +137,7 @@ void            Threads::_threadFinished()
     }
 }
 
-void            Threads::_threadDestroyed(QThread *object)
+void            Threads::_threadDestroyed(QObject *object)
 {
     SmartMutex  mutex(this->mutex, "Threads", "_threadDestroyed");
 