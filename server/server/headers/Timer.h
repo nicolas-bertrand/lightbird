1c6bf57093fd9b70402712276a05c759904d1760
Nicolas <contact@nicolasbertrand.fr>
2011-07-23 21:45:49 +0200

    Documents Timer.h.

--- server/server/headers/Timer.h
+++ server/server/headers/Timer.h
@@ -6,6 +6,9 @@
 
 class ApiTimers;
 
+/// @brief Manages one timer of a plugin. The events of the timer occure in a
+/// specific thread, created by this class. The timers are manage by ApiTimers.
+/// @see LightBird::ITimer
 class Timer : public QThread
 {
     Q_OBJECT
@@ -16,22 +19,27 @@ public :
     Timer(const Timer &timer);
     Timer &operator=(const Timer &timer);
 
+    ///< @brief The main method of the timer thread.
     void        run();
+    ///< @brief Returns the interval between each events.
     unsigned    getTimeout();
+    ///< @brief Allows to modify the timeout of the timer.
     void        setTimeout(unsigned int timeout);
+    ///< @brief Stops the timer.
     void        stop();
 
 private slots:
+    ///< @brief This slot is called for each timeout and calls LightBird::ITimer.
     void        _timeout();
 
 private:
     Timer();
 
-    QString     id;
-    QString     name;
-    unsigned    timeout;
-    ApiTimers   &apiTimers;
-    bool        stopped;
+    QString     id;         ///< The id of the plugin.
+    QString     name;       ///< The name of the timer.
+    unsigned    timeout;    ///< The number of milliseconds between each call to _timeout.
+    ApiTimers   &apiTimers; ///< Manages all the timers of a plugin.
+    bool        stopped;    ///< True if the timer have to stop.
 };
 
 #endif // TIMER_H
