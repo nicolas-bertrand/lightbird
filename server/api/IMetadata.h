b314eeca9d76f015223f838d8c079efca5089b69
Nicolas <contact@nicolasbertrand.fr>
2012-10-18 10:33:44 +0200

    Adds dependencies to IMetadata.

--- server/api/IMetadata.h
+++ server/api/IMetadata.h
@@ -1,23 +1,24 @@
 #ifndef IMETADATA_H
 # define IMETADATA_H
 
-# include <QMap>
 # include <QString>
+# include <QVariantMap>
 
 namespace LightBird
 {
     /// @brief Stores information about a plugin.
     struct                     IMetadata
     {
-        QString                name;        ///< The real name of the plugin.
-        QString                brief;       ///< A brief description of its features.
-        QString                description; ///< A more detailled description.
-        QString                autor;       ///< The autor of the plugin.
-        QString                site;        ///< The site that support the plugin.
-        QString                email;       ///< Email of the autor.
-        QString                version;     ///< The version of the plugin.
-        QString                licence;     ///< The licence under which the plugin is distributed.
-        QMap<QString, QString> other;       ///< Other relevant information.
+        QString     name;         ///< The full name of the plugin.
+        QString     brief;        ///< A brief description of its features.
+        QString     description;  ///< A more detailled description.
+        QString     autor;        ///< The autor of the plugin.
+        QString     site;         ///< The site that support the plugin.
+        QString     email;        ///< Email of the autor.
+        QString     version;      ///< The version of the plugin.
+        QString     licence;      ///< The licence under which the plugin is distributed.
+        QStringList dependencies; ///< List the dependances of the plugin (i.e. third-party libraries).
+        QVariantMap other;        ///< Other relevant information.
     };
 }
 
--- server/plugins/Tls/Plugin.cpp
+++ server/plugins/Tls/Plugin.cpp
@@ -86,6 +86,7 @@ void    Plugin::getMetadata(LightBird::IMetadata &metadata) const
     metadata.email = "team@lightbird.cc";
     metadata.version = "1.0";
     metadata.licence = "CC BY-NC-SA 3.0";
+    metadata.dependencies << QString("GnuTLS %1").arg(GNUTLS_VERSION);
 }
 
 bool    Plugin::onConnect(LightBird::IClient &client)
